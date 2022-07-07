2ecf1124b6a770803d1a61dea4a881c0a2fced24
Szymon Buda <szymonbuda1996@gmail.com>
2022-07-07 13:15:12 +0200

    first commit

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+            <header>
+                <div className="logo">Logo</div>
+                <nav>
+                    <div>About me</div>
+                    <div>My projects</div>
+                    <div>Contact</div>
+                </nav>
+            </header>
+    );
+};
